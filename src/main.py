5ba7ac441e793eba983e688904559f5fd43cf2bd
SprintKey <86112157+SprintKey@users.noreply.github.com>
2023-03-24 16:26:50 +0300

    put the main text and positioned it

--- src/main.py
+++ src/main.py
@@ -5,4 +5,6 @@ root.title("Freegames Key Generator")
 root.geometry("500x400")
 root.configure(bg="grey17")
 
+main_text = Label(root, text="Which Game do you want??").grid(row=0,column=2)
+
 root.mainloop()
